d1eac92f393bbdc673923c2032398cf66334cc6b
kravindra1415 <kravindra1415@gmail.com>
2022-11-02 16:52:49 +0530

    Add project files.

---
+++ WebApplication1/Controllers/CityController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "pune", "mumbai" };
+        }
+
+        [HttpGet("{id}")]
+        public String GetById(int id)
+        {
+            return "abc";
+        }
+    }
+}
